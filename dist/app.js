3a6fc637c2981fe45fbe01ba1f00a5e7cca325a2
Sergey Antipov <antip99999@mail.ru>
2023-03-17 13:17:53 +0300

    Search-component layout

--- dist/app.js
+++ dist/app.js
@@ -1032,7 +1032,6 @@
 		}
 
 		render() {
-			this.el.innerHTML = '';
 			this.el.classList.add('header');
 			this.el.innerHTML = `
 			<div>
@@ -1051,6 +1050,32 @@
 					</div>
 				</a>
 			</div>
+		`;
+			return this.el;
+		}
+	}
+
+	class Search extends DivComponent {
+		constructor(state) {
+			super();
+			this.state = state;
+		}
+
+		render() {
+			this.el.classList.add('search');
+			this.el.innerHTML = `
+			<div class='search__wrapper'>
+				<input 
+					type='text'
+				 	placeholder='Найти книгу или автора....'
+					class='search__input'
+					value='${this.state.searchQuery ? this.state.searchQuery : ''}'
+				/>
+				<img src='/static/search.svg' alt='Search'/>
+			</div>
+			<button aria-label='Search'>
+				<img src='/static/search-white.svg' alt='Search'/>
+			</button>
 		`;
 			return this.el;
 		}
@@ -1078,6 +1103,7 @@
 
 		render() {
 			const main = document.createElement('div');
+			main.append(new Search(this.state).render());
 			this.app.innerHTML = '';
 			this.app.append(main);
 			this.renderHeader();
